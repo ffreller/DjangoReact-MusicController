8644f10307f351fe0982cb5476459304896eeb4a
Fabio Freller <f.freller@gmail.com>
2022-03-30 15:33:03 +0000

    2

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from string import ascii_uppercase
+from random import choices
+
+def generate_unique_code():
+    lentgh = 6
+    while True:
+        code = ''.join(choices(ascii_uppercase, k=lentgh))
+        if Room.objects.filter(code=code).count() == 0:
+            return code
 
 # Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=8, default='', unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
