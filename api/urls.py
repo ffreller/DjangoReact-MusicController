266cd75ead25716e375ab5c9f7ff4c13bfb80ebc
Fabio Freller <f.freller@gmail.com>
2022-04-01 13:44:31 +0000

    8 inteiro

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RoomView
+from .views import GetRoom, RoomView, CreateRoomView, JoinRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
+    path('create_room', CreateRoomView.as_view()),
+    path('get_room', GetRoom.as_view()),
+    path('join_room', JoinRoom.as_view()),
 ]
